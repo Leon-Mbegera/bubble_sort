f992aa1692903ba7e9184facb01022d28d430302
shoirata <shoira.shakirovna@gmail.com>
2021-03-19 13:39:23 +0500

    define logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,26 @@
 # defining bubble_sort method
 
-def bubble_sort(array)
-  
+def bubble_sort(arr)
+  #[9, 2, 8]
+  #[2, 9, 8]
+  #[2, 8, 9]
+  n = arr.length
+  loop do
+  swapped =false
 
+     arr.each do |i|
+         
+          if arr[i] > arr[i+1]
+          #arr[i], arr[i+1] = arr[i+1], arr[i]
+          temp =  arr[i] 
+          arr[i] = arr[i+1]
+          arr[i+1]= temp
+          end
+     end
+     break if not swapped
 
-return array          # returns array
+  end
+return arr          # returns array
 end
 
 puts bubble_sort([9, 2, 8])
