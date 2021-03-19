58d98f265f9794a2500129c69a3fb53b9e9ad860
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-19 13:07:55 +0300

    add yield bubble sort

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,4 +1,20 @@
 # defining bubble_sort method
-def bubble_sort(arr)
+def bubble_sort_by(arr)
+  n = arr.length
+  loop do
+  swapped =false
+     (n-1).times do |i|
+          if yield(arr[i], arr[i + 1]).positive?
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+             swapped = true
+          end
+     end
+     break if not swapped
+  end
+  return arr  
+end   
 
+sorted_array = bubble_sort_by(["hey", "hello", "hi"]) do |left, right|
+  left.length - right.length
 end
+puts sorted_array
