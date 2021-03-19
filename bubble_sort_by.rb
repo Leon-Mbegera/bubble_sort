c152896d23eb3d629877074b6e566c7a87f3363a
shoirata <shoira.shakirovna@gmail.com>
2021-03-19 17:44:00 +0500

    fix: rubocop err

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,18 +1,18 @@
 def bubble_sort_by(arr)
   n = arr.length
   loop do
-  swapped =false
-     (n-1).times do |i|
-          if yield(arr[i], arr[i + 1]).positive?
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-             swapped = true
-          end
-     end
-     break if not swapped
+    swapped = false
+    (n - 1).times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    break unless swapped
   end
-  return arr  
-end   
-sorted_array = bubble_sort_by(["hey", "hello", "hi"]) do |left, right|
+  arr
+end
+sorted_array = bubble_sort_by(%w[hey hello hi]) do |left, right|
   left.length - right.length
 end
 puts sorted_array
